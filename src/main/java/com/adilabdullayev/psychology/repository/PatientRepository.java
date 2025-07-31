28488785d413089cf145fc1505fc3886ff52df4e
adilabdev <adileleddinoglu@gmail.com>
2025-08-01 00:09:51 +0300

    added findByemail and findbyPhone Optional searching

--- src/main/java/com/adilabdullayev/psychology/repository/PatientRepository.java
+++ src/main/java/com/adilabdullayev/psychology/repository/PatientRepository.java
@@ -2,6 +2,13 @@ package com.adilabdullayev.psychology.repository;
 
 import com.adilabdullayev.psychology.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface PatientRepository extends JpaRepository<Patient, Long>{
+
+    Optional<Patient> findByEmail(String email);
+    Optional<Patient> findByPhone(String phone);
 }
