72e1f1a989575aa9cae9bacdbb58dd1d54c51039
adilabdev <adileleddinoglu@gmail.com>
2025-08-01 23:45:46 +0300

    Created initial counselor controller

---
+++ src/main/java/com/adilabdullayev/psychology/contoller/CounselorController.java
@@ -0,0 +1,28 @@
+package com.adilabdullayev.psychology.contoller;
+
+import com.adilabdullayev.psychology.model.Counselor;
+import com.adilabdullayev.psychology.service.CounselorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/counselors")
+public class CounselorController {
+    private final CounselorService service;
+
+    public CounselorController(CounselorService service){
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Counselor> getAll(){
+        return service.getAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Counselor> create(@RequestBody Counselor counselor){
+        return ResponseEntity.ok(service.add(counselor));
+    }
+}
