5505f65fdf5951529cd5df4afadd80f9799c09c0
adilabdev <adileleddinoglu@gmail.com>
2025-08-07 04:10:19 +0300

    added existsByPatientCode method to check uniqueness of patient codes

--- src/main/java/com/adilabdullayev/psychology/repository/PatientRepository.java
+++ src/main/java/com/adilabdullayev/psychology/repository/PatientRepository.java
@@ -4,6 +4,9 @@ import com.adilabdullayev.psychology.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 
 @Repository
@@ -12,4 +15,7 @@ public interface PatientRepository extends JpaRepository<Patient, Long>, Patient
     Optional<Patient> findByPhone(String phone);
     Optional<Patient> findByEmailOrPhone(String email, String phone);   //to get all (deleted and undeleted)
     Optional<Patient> findByEmailOrPhoneAndDeletedFalse(String email, String phone); //to get only undeleted
+
+    boolean existsByPatientCode(String patientCode);
+
 }
