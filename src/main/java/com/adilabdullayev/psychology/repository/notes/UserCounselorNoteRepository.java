ccb88114b7482bdca9e874caa1693291248e9740
adilabdev <adileleddinoglu@gmail.com>
2025-08-07 04:54:10 +0300

    findByPatientIdAndContentContainingIgnoreCase: search notes by patient ID and keyword (case-insensitive) findByPatientIdAndTypeAndContentContainingIgnoreCase: search notes by patient ID, note type, and keyword (case-insensitive) Enables flexible keyword search filtering by note type or across all types

--- src/main/java/com/adilabdullayev/psychology/repository/notes/UserCounselorNoteRepository.java
+++ src/main/java/com/adilabdullayev/psychology/repository/notes/UserCounselorNoteRepository.java
@@ -6,5 +6,12 @@ import java.util.List;
 
 public interface UserCounselorNoteRepository extends JpaRepository<UserCounselorNote, Long> {
     List<UserCounselorNote> findByPatientId(Long patientId);
+
     List<UserCounselorNote> findByPatientIdAndType(Long patientId, UserCounselorNote.NoteType type);
+
+    // Intensive Search word based
+    List<UserCounselorNote> findByPatientIdAndContentContainingIgnoreCase(Long patientId, String keyword);
+
+    List<UserCounselorNote> findByPatientIdAndTypeAndContentContainingIgnoreCase(Long patientId, UserCounselorNote.NoteType type, String keyword);
+
 }
