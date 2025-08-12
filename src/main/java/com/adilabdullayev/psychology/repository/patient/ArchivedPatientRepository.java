8413268d12f1c27cdec20d9fa8509d2fd4bce791
adilabdev <adileleddinoglu@gmail.com>
2025-08-12 17:54:00 +0300

    created: added ArchivedPatientRepository for managing archived patient records
    - Implemented methods to delete, check existence, and find archived patients by email or phone
    - Added @Transactional and @Modifying for delete query

---
+++ src/main/java/com/adilabdullayev/psychology/repository/patient/ArchivedPatientRepository.java
@@ -0,0 +1,27 @@
+package com.adilabdullayev.psychology.repository.patient;
+
+import com.adilabdullayev.psychology.model.patient.ArchivedPatients;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+@Repository
+public interface ArchivedPatientRepository extends JpaRepository<ArchivedPatients, Long> {
+
+    // deletes client by stade email or phone number
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM ArchivedPatients a WHERE a.email = :email OR a.phone = :phone")
+    void deleteByEmailOrPhone(String email, String phone);
+
+    // checks if there is a record by specific email address or phone number
+    boolean existsByEmailOrPhone(String email, String phone);
+
+    // returns client informations by specific email address or phone number
+    Optional<ArchivedPatients> findByEmailOrPhone(String email, String phone);
+
+}
